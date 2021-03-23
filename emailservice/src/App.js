bb4b78a5526726ab1667ded7b4c3c3e51fa81bc6
Kritika <kaur.kritika.kritika@gmail.com>
2021-03-23 23:35:55 +0530

    EmailList

--- emailservice/src/App.js
+++ emailservice/src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Sidebar from './Sidebar'
+import EmailList from "./EmailList";
+import Mail from './Mail';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
+    <Router>
     <div className="app">
     <Header />
+    <div className = "app_body">
     <Sidebar />
+    <Switch>
+    <Route path = "/mail"><Mail /></Route>
+    <Route path = "/"><EmailList /></Route>
+    </Switch>
     </div>
+    </div>
+    </Router>
   );
 }
 
