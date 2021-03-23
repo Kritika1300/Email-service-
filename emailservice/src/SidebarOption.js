c79fbc8fe4836a20bfdd3aa5fcaf52d0aaed14ea
Kritika <kaur.kritika.kritika@gmail.com>
2021-03-23 22:51:46 +0530

    Sidebar

---
+++ emailservice/src/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SidebarOption.css';
+function SidebarOptions({Icon, title , number, selected}) {
+    return (
+        <div className = {`sidebarOption ${selected && "sidebarOption--active"}`}>
+        <Icon />
+        <h3>{title}</h3>
+        <p>{number}</p>
+        </div>
+    )
+}
+export default SidebarOptions;
