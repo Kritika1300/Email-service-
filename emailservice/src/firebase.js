a870fe2de49670448ff9490ecd864112c85af6f3
Kritika <kaur.kritika.kritika@gmail.com>
2021-04-07 15:47:13 +0530

    Firebase.json

---
+++ emailservice/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from  'firebase';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCE6fLPdSMBSRH7vA2WYjHbGCNl9pCeci0",
+    authDomain: "email-client-5b9d0.firebaseapp.com",
+    projectId: "email-client-5b9d0",
+    storageBucket: "email-client-5b9d0.appspot.com",
+    messagingSenderId: "650645615578",
+    appId: "1:650645615578:web:0cf2e10b352d31b6d406e3",
+    measurementId: "G-1EDKRZRPQE"
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
