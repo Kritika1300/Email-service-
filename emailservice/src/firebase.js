7cf213273c022078ca9a5420b80d4a5703375b0e
Kritika <kaur.kritika.kritika@gmail.com>
2021-04-07 20:57:19 +0530

    Adding emails to firebase collection

--- emailservice/src/firebase.js
+++ emailservice/src/firebase.js
@@ -12,5 +12,5 @@ const firebaseConfig = {
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
-  const  provider = new firebaseApp.auth.GoogleAuthProvider();
+  const  provider = new firebase.auth.GoogleAuthProvider();
   export {db,auth,provider};
