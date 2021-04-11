1bfbd0d164c965acacf86637e2bc12d8acaac4f2
Kritika <kaur.kritika.kritika@gmail.com>
2021-04-11 15:45:26 +0530

    changes

--- emailservice/src/EmailRow.js
+++ emailservice/src/EmailRow.js
@@ -1,28 +1,48 @@
 import { Checkbox, IconButton } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './EmailRow.css';
 import LabelImportantOutlinedIcon from '@material-ui/icons/LabelImportantOutlined';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { useHistory } from 'react-router';
 import { selectMail } from './features/mailSlice';
 import { useDispatch } from 'react-redux';
-function EmailRow({ id, title, subject, description, time }) {
+import {db} from './firebase';
+
+function EmailRow({ id, title, subject, description, time, starred, important }) {
   const history = useHistory();
   const dispatch = useDispatch();
   const openMail = () => {
     dispatch(selectMail({
-      id, title, subject, description, time
+      id, title, subject, description, time, important
     }));
     history.push("/mail")
   };
+  const [starMail, setStarMail] = useState(starred);
+  const [imp,setImpMail] = useState(important);
+  const starMailFunc = () => {
+    setStarMail((oldval) => {
+      db.collection("emails").doc(id).update({
+        starred: !oldval
+      })
+      return !oldval;
+    });
+  }
+  const impMail = () => {
+    setImpMail((oldval) => {
+      db.collection("emails").doc(id).update({
+        important : !oldval
+      })
+      return !oldval;
+    })
+  }
   return <div className="emailRow">
     <div className="emailRow_options">
       <Checkbox />
       <IconButton>
-        <StarBorderIcon />
+        <StarBorderIcon onClick = { starMailFunc } className={starMail && "starred"}/>
       </IconButton>
       <IconButton>
-        <LabelImportantOutlinedIcon />
+        <LabelImportantOutlinedIcon onClick = {impMail} className = {imp && "important"}/>
       </IconButton>
     </div>
     <h3 onClick={openMail} className="emailRow_title">
